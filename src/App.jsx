be39f3f79f08a34897f34225141914a3a1b81fe1
Roman Fedevych <fedevychr@gmail.com>
2024-03-22 00:51:35 +0200

    add app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Description from './components/Description/Description';
+
+function App() {
+  return (
+    <div>
+      <>
+        <Description />
+      </>
+    </div>
+  );
+}
+
+export default App;
