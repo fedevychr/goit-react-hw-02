a46fa3f6686ee36ab6329bac67efbce372b8dc08
Roman Fedevych <fedevychr@gmail.com>
2024-03-22 17:53:43 +0200

    fin step6 and task

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,35 @@ import Description from './components/Description/Description';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
 import Feedback from './components/Feedback/Feedback';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const initialFeedbacks = { good: 0, neutral: 0, bad: 0 };
 
 function App() {
-  const [feedbacks, setFeedbacks] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedbacks, setFeedbacks] = useState(() => {
+    const stringifiedFeedbacks = localStorage.getItem('feedbacksValues');
+    const parsedFeedbacks =
+      JSON.parse(stringifiedFeedbacks) ?? initialFeedbacks;
+    return parsedFeedbacks;
+  });
 
   const updateFeedback = feedbackType => {
     setFeedbacks({ ...feedbacks, [feedbackType]: feedbacks[feedbackType] + 1 });
   };
 
   const resetFeedback = () => {
-    setFeedbacks({ good: 0, neutral: 0, bad: 0 });
+    setFeedbacks(initialFeedbacks);
   };
 
   const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
-  console.log('totalFeedback: ', totalFeedback);
 
   const positiveFeedback = Math.round(
     ((feedbacks.good + feedbacks.neutral) / totalFeedback) * 100,
   );
+
+  useEffect(() => {
+    localStorage.setItem('feedbacksValues', JSON.stringify(feedbacks));
+  }, [feedbacks]);
 
   return (
     <div>
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+
 const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
+  useEffect(() => {
+    const onKeyDown = event => {
+      if (event.code === 'Reset') {
+        return;
+      }
+    };
+
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [resetFeedback]);
+
   return (
     <div>
       <button onClick={() => updateFeedback('good')}>Good</button>
